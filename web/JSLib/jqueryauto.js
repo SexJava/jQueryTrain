fc511e1eb9178ebe814901bf7af96f2fea64c7ba
SexJava <man021436@163.com>
2019-10-04 19:42:48 +0800

    提交学习jQuery源码

--- web/JSLib/jqueryauto.js
+++ web/JSLib/jqueryauto.js
@@ -4,10 +4,8 @@ $(document).ready(function () {
     $("#auto").hide();
     //给文本框加键盘按下并谈起的时间
     $("#word").keyup(function () {
-
     });
     //给按钮添加时间,表示文本框中的数据被提交了
     $("input[type='button']").click(function () {
-        
     })
 });
